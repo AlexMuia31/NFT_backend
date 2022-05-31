9a9b4d2b484db5f430c83d4af366129770b51fc9
AlexMuia31 <alexmuia31@gmail.com>
2022-05-31 13:39:01 +0300

    crypto devs smart contract

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -4,3 +4,15 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IWhitelist.sol";
+
+contract CryptoDevs is ERC721Enumerable, Ownable {
+    string _baseTokenURI;
+    IWhitelist whitelist;
+
+    constructor(string memory _baseURI, address whitelistContract)
+        ERC721("Crypto Devs", "CD")
+    {
+        _baseTokenURI = _baseURI;
+        whitelist = IWhitelist(whitelistContract);
+    }
+}
