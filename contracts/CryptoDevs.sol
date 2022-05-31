e70c46d4c1d9d2a0e265b276a7278948a55f25fb
AlexMuia31 <alexmuia31@gmail.com>
2022-05-31 13:38:37 +0300

    crypto devs smart contract

---
+++ contracts/CryptoDevs.sol
@@ -0,0 +1,6 @@
+//SPDX-Lincese_Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "./IWhitelist.sol";
