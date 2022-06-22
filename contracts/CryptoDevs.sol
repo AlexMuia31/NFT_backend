7b5b0b7b592c29a77171d09da0ae12c1d2270b37
AlexMuia31 <alexmuia31@gmail.com>
2022-06-22 13:33:34 +0300

    comments

--- contracts/CryptoDevs.sol
+++ contracts/CryptoDevs.sol
@@ -6,9 +6,21 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 import "./IWhitelist.sol";
 
 contract CryptoDevs is ERC721Enumerable, Ownable {
+    /**
+     * @dev _baseTokenURI for computing {tokenURI}. If set, the resulting URI for each
+     * token will be the concatenation of the `baseURI` and the `tokenId`.
+     */
     string _baseTokenURI;
+
+    // Whitelist contract instance
     IWhitelist whitelist;
 
+    /**
+     * @dev ERC721 constructor takes in a `name` and a `symbol` to the token collection.
+     * name in our case is `Crypto Devs` and symbol is `CD`.
+     * Constructor for Crypto Devs takes in the baseURI to set _baseTokenURI for the collection.
+     * It also initializes an instance of whitelist interface.
+     */
     constructor(string memory _baseURI, address whitelistContract)
         ERC721("Crypto Devs", "CD")
     {
